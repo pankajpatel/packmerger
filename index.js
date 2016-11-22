6b6117abeade879656eb1486e0ccae0bf61689a4
Pankaj Patel <patel.pankaj@live.in>
2016-11-22 23:18:59 +0530

    file supply working

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var readJson = require('read-package-json')
 var argv = require('minimist')(process.argv.slice(2))
 var program = require('commander');
@@ -5,22 +7,31 @@ var pkg = require('./package');
 
 program
   .version(pkg.version)
-  .arguments('<url> [output-dir]')
+  .arguments('<primary> [files...]')
   .option('-o, --out', 'Output to file')
   .option('-f, --force', 'Overwriting existing files')
-  .action((url, output) => {
-    urlValue = url
-    outputDir = output ? path.resolve(output) : process.cwd()
+  .action((primary, files) => {
+    // urlValue = url
+    // outputDir = output ? path.resolve(output) : process.cwd()
+    console.log(primary, files)
+    getData()
   })
 program.parse(process.argv)
 
+if (process.argv.slice(2).length < 2) {
+  program.outputHelp()
+  process.exit()
+}
 
-// readJson(filename, [logFunction=noop], [strict=false], cb)
-readJson('./package.json', console.error, false, function (er, data) {
-  if (er) {
-    console.error("There was an error reading the file")
-    return
-  }
 
-  console.error('the package data is', data)
-});
+function getData() {
+  // readJson(filename, [logFunction=noop], [strict=false], cb)
+  readJson('./package.json', console.error, false, function (er, data) {
+    if (er) {
+      console.error("There was an error reading the file")
+      return
+    }
+
+    console.error('the package data is', data)
+  });
+}
