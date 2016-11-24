d993eb6189a4df39338302f3853cb0f267f2446a
Pankaj Patel <patel.pankaj@live.in>
2016-11-24 23:07:57 +0530

    all files' contents are obtained

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var argv = require('minimist')(process.argv.slice(2))
 var program = require('commander');
 var pkg = require('./package');
 
+var allData = {};
+var count = 0;
+var total = 0;
+
 program
   .version(pkg.version)
   .arguments('<primary> [files...]')
@@ -14,6 +18,7 @@ program
     // urlValue = url
     // outputDir = output ? path.resolve(output) : process.cwd()
     console.log(primary, files)
+    total = files.length + 1;
     getData(primary, files)
   })
 program.parse(process.argv)
@@ -23,15 +28,37 @@ if (process.argv.slice(2).length < 1) {
   process.exit()
 }
 
-
 function getData(primary, files) {
   // readJson(filename, [logFunction=noop], [strict=false], cb)
-  readJson(primary, console.error, false, function (er, data) {
+  readJsonFile(primary, prepareData)
+  for (var i = files.length - 1; i >= 0; i--) {
+    readJsonFile(files[i], prepareData)
+  }
+}
+
+function readJsonFile(file, callback) {
+  readJson(file, console.error, false, function (er, data) {
     if (er) {
       console.error("There was an error reading the file")
-      return
+      return;
     }
-
-    console.error('the package data is', data)
+    callback(file, data)
   });
+}
+
+function prepareData(file, data, callback) {
+  allData[file] = data;
+  count++;
+
+  if( callback ){
+    callback()
+  }
+
+  if(count == (total) ){
+    finallyDoMerge()
+  }
+}
+
+function finallyDoMerge() {
+  console.log(count, allData)
 }
